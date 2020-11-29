106896587540b88ab2d1b25de379b0cbb73dbdf5
xv994 <74281061+xv994@users.noreply.github.com>
2020-11-29 19:44:40 +0800

    Add files via upload

--- get_gradient.py
+++ get_gradient.py
@@ -1,6 +1,13 @@
 import lost
 
-def get_gradient(k):
+
+def get_k_gradient(train_data, k, b):
     step = 0.0000000001  # 1e-10
-    gradient = (lost(k + step) - lost(k)) / step
+    gradient = (lost(train_data, k + step, b) - lost(train_data, k, b)) / step
+    return gradient
+
+
+def get_b_gradient(train_data, k, b):
+    step = 0.0000000001  # 1e-10
+    gradient = (lost(train_data, k, b + step) - lost(train_data, k, b)) / step
     return gradient
